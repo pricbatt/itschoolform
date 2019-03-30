ab36b43467c585377d5812f3d683e5ff6b4ee507
ppraserts <ppraserts@gmail.com>
2019-03-31 00:02:06 +0700

    test connection mysql

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    phpinfo();
+    $servername = "localhost";
+    $username = "mfu";
+    $password = "Mfuworkshop@2019";
+    $dbname = "mfuworkshop";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student List</title>
+</head>
+<body>
+    
+</body>
+</html>

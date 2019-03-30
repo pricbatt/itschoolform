986f3c7d70afca1afcd38a6e5543d4890b7c60bf
ppraserts <ppraserts@gmail.com>
2019-03-31 00:35:38 +0700

    fix thai problem mysql

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
+    $conn->set_charset("utf8");
     $sql = "SELECT * FROM Student";
     $result = $conn->query($sql);
 ?>

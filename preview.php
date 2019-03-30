308b280ace19027e574e73958f5b50c1ebfd6a6c
ppraserts <ppraserts@gmail.com>
2019-03-31 01:59:36 +0700

    add home button

---
+++ preview.php
@@ -0,0 +1,14 @@
+<?php
+// Require composer autoload
+require_once __DIR__ . '/vendor/autoload.php';
+$mpdf = new \Mpdf\Mpdf();
+
+$pagecount = $mpdf->SetSourceFile("/documents/MFU-P09_000132 - Original.pdf");
+for ($i=1; $i<=$pagecount; $i++) {
+    $mpdf->AddPage();
+    $import_page = $mpdf->ImportPage($i);
+    //$mpdf->Image('sign.jpg', 0, 0, 210, 297, 'jpg', '', true, false);
+    $mpdf->UseTemplate($import_page);
+}
+//$mpdf->WriteHTML();
+$mpdf->Output();
